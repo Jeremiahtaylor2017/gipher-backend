81a8fb72449dbfd208206cf6a046f9e9cc575347
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2022-08-26 01:24:07 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+// dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const logger = require("morgan");
+require("dotenv").config();
+
+// setup
+const app = express();
+const { PORT, MONGO_URI } = process.env;
+
+// db connections
+mongoose.connect(MONGO_URI);
+mongoose.connection
+	.on("open", () => console.log("Connected to MongoDB"))
+	.on("close", () => console.log("Disconnected from MongoDB"))
+	.on("error", () => console.log(err.message));
+
+// mount middleware
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+	res.send("Gipher API");
+});
+
+// listener
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
